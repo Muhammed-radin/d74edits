2af32b1593938e45c59222cd15a921a87a7f6f3a
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2023-07-08 22:31:16 +0530

    options added

--- explore/explore.js
+++ explore/explore.js
@@ -6,23 +6,47 @@ db.getYTchannelVideos(function(xhr = new XMLHttpRequest()) {
   }
 
   res.items.forEach(function(data, index) {
+    var id = 'ID_10' + index
     var snippet = data.snippet
-    var code = `<div class="card">
+    var code = `<div class="card" id="${id}">
       <div class="card-date">${snippet.publishedAt.slice(0, 10)}</div>
       <div class="card-img">
         <img src="${snippet.thumbnails.medium.url}" alt="yt">
       </div>
       <div class="card-title">${snippet.title}</div>
-      <div class="card-opt-icon">
+      <div class="card-opt-icon" onclick="openOptions('${id+"2"}')">
         <ion-icon name="ellipsis-horizontal"></ion-icon>
       </div>
-      <!div class="card-options">
-        <! /div>
+      <div class="card-options" id="${id+"2"}">
+        <div class="card-head">
+          Options
+        </div>
+        <div class="card-body">
+          <p class="card-opt">
+            View
+          </p>
+          <p class="card-opt">
+            Save
+          </p>
+          <p class="card-opt" onclick="closeOptions('${id+'2'}')">
+            Close
+          </p>
+        </div>
     </div>
 `
     document.querySelector('.body').innerHTML += code
   })
 })
+
+function openOptions(id) {
+  var elem = document.getElementById(id)
+  elem.style.display = 'block'
+}
+
+function closeOptions(id) {
+  var elem = document.getElementById(id)
+  elem.style.display = 'none'
+}
 
 if (localStorage.getItem('user')) {
 
