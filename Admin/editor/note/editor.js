1dc766fc6f350d5bbed48d8e099d9ada3272ff67
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2023-07-09 17:41:27 +0530

    basic script added

--- Admin/editor/note/editor.js
+++ Admin/editor/note/editor.js
@@ -3,6 +3,7 @@ var mng = canvas;
 var mango = canvas;
 var cnv = canvas.setCanvas(document.getElementById('c'));
 var ctx = cnv.ctx;
+var elem = document
 var elem = cnv.elem;
 var entity = canvas.entity;
 var entityGroup = canvas.entityGroup;
@@ -13,6 +14,27 @@ elem.style.border = '1px solid'
 
 canvas.setWidth(330, 350)
 
+var nowTool = 'move'
+var touchstart = 0
+var touchend = 0
 
+function move(e){
+  switch (nowTool) {
+    case 'move':
+      
+      break;
+  }
+}
+
+
+elem.addEventListener('touchstart', function(e){
+  touchstart = e
+  elem.addEventListener('touchmove', move)
+})
+
+elem.addEventListener('touchstart', function(e){
+  touchend = e
+  elem.removeEventListener('touchmove', move)
+})
 
 canvas.specialRender()
