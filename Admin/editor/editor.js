9901b089d103caa9fe5c0d6a1bce9d95ae306291
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2023-07-09 17:16:59 +0530

    mangojs started

---
+++ Admin/editor/editor.js
@@ -0,0 +1,16 @@
+var canvas = Mango();
+var mng = canvas;
+var mango = canvas;
+var cnv = canvas.setCanvas(document.getElementById('c'));
+var ctx = cnv.ctx;
+var elem = cnv.elem;
+var entity = canvas.entity;
+var entityGroup = canvas.entityGroup;
+var layer = canvas.Layer;
+var app = canvas.app
+
+canvas.fillScreen()
+
+
+
+canvas.repeatRender()
