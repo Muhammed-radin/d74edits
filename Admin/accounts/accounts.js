6b8848f71ddcbaee87cddb39e30aec2c51b1d4f4
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2023-07-16 22:15:56 +0530

    ui updated

--- Admin/accounts/accounts.js
+++ Admin/accounts/accounts.js
@@ -2,6 +2,12 @@ db.get('account', function(xhr) {
   if (xhr.status == 0) {
     alert('Server Error')
   }
+  
+  document.querySelector('.body').innerHTML += `<p class="acc gr">
+      <ion-icon name="people-outline"></ion-icon>
+      <sp class="sp">${JSON.parse(xhr.response).length} Accounts</sp>
+      <span class="sp">[ ${xhr.status} ]</span>
+    </p>`
 
   JSON.parse(xhr.response).forEach(function(acc) {
     document.querySelector('.body').innerHTML += `<p class="acc">
