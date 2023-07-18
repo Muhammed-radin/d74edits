f66ddea1c85ad5c76ead9e5fc5c5f3352cbfd9ec
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2023-07-18 15:19:24 +0530

    updated

--- Admin/edit_video/edit_videos.js
+++ Admin/edit_video/edit_videos.js
@@ -8,11 +8,34 @@ if (params == '') {
 } else {
   var objectParams = JSON.parse(paramsString);
   objectParams = objectParams
+
   if (objectParams.id == undefined) {
 
   } else {
     var ytId = objectParams.id
-    
-    //db.get('videos')
+    var q = 'videos?q=' + decodeURI(JSON.stringify({ ytid: ytId }))
+
+    db.get(q, function(xhr) {
+      var res = JSON.parse(xhr.response)[0]
+
+      document.getElementById('title').value = res.title
+      document.getElementById('password').value = res.pw
+      document.getElementById('date').value = res.date
+      document.getElementById('ytId').value = res.ytid
+      document.getElementById('sid').value = res._id
+      document.getElementById('vurl').value = res.url
+      document.getElementById('imgUrl').value = res.thumb
+
+    })
   }
 }
+
+function loadImg() {
+  document.getElementById('img').src = document.getElementById('imgUrl').value
+  document.getElementById('img').onerror = function() {
+    document.getElementById('img').src = '../../visit/image.png'
+  }
+}
+
+document.getElementById('imgUrl').addEventListener('change', loadImg);
+document.getElementById('imgUrl').addEventListener('keyup', loadImg);
