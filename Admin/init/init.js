1acbfcac0bdadae6fb8982bc5594a05f237c56c7
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2023-07-19 20:31:31 +0530

    installation page added

--- Admin/init/init.js
+++ Admin/init/init.js
@@ -16,9 +16,50 @@ if (params == '') {
   } else {
     var ytId = objectParams.id
 
-    sub = document.getElementById('sub')
-    sug.innerHTML = 'Getting...'
-    
-    
+    var sub = document.getElementById('sub')
+    sub.innerHTML = 'Getting...'
+
+    var loader = document.getElementById('load')
+
+    const data = null;
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
+    xhr.addEventListener('readystatechange', function() {
+      if (this.readyState === this.DONE) {
+        if (xhr.status != 200) {
+          sub.innerHTML = 'ERROR: ' + this.status + ' -- ' + this.statusText
+          load.value = 0
+        } else {
+          sub.innerHTML = 'Installing...'
+          load.value = 50
+          
+          var data = JSON.parse(xhr.response).items[0]
+          var snippet = data.snippet
+          
+
+          db.push('videos', JSON.stringify({
+            thumb: snippet.thumbnails.medium.url,
+            date: snippet.publishedAt,
+            ytid: ytId,
+            pw: 'd4f2cwyw7',
+            description: snippet.localized.description.slice(0, 200),
+            url: 'https://youtube.com/watch?v=' + ytId,
+            title: snippet.title,
+          }), function (xhr2 = new XMLHttpRequest()){
+            if (xhr2.status != 201) {
+              sub.innerHTML = 'Server Upload Failed '+xhr2.status
+            } else {
+              sub.innerHTML = 'Successfully Installed'
+              loader.value = 100
+              
+            }
+          })
+        }
+      }
+    });
+    xhr.open('GET', 'https://youtube-v31.p.rapidapi.com/videos?part=contentDetails%2Csnippet%2Cstatistics&id='+ytId);
+    xhr.setRequestHeader('X-RapidAPI-Key', '786ab693aamsh13b490be165befdp1ebb1bjsn20906d36a500');
+    xhr.setRequestHeader('X-RapidAPI-Host', 'youtube-v31.p.rapidapi.com');
+    xhr.send(data);
   }
 }
