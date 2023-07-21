18cf0d25985e401153f6a93ebd1531862b2b36d3
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2023-07-21 23:56:16 +0530

    login page updated

--- login/login.js
+++ login/login.js
@@ -9,23 +9,34 @@ class UserDataModel {
   }
 }
 
-if (localStorage.getItem('user')) {
-  if (user.name.includes('guest') && user.email.includes('guest')) {
-    if (user.id == undefined) {
-      setText('Updating...')
-      loadit()
-      db.get('account?q=' + decodeURI(JSON.stringify({ name: user.name, email: user.email })), function(xhr) {
-        var res = JSON.parse(xhr.response)
-        localStorage.setItem('user', JSON.stringify(new UserDataModel(user.name, user.email, res._id, JSON.stringify(res))))
-        unload()
-      })
+function logIt() {
+  if (localStorage.getItem('user')) {
+    if (user.name.includes('guest') && user.email.includes('guest')) {
+      if (user.id == undefined) {
+        setText('Updating...')
+        loadit()
+        db.get('account?q=' + decodeURI(JSON.stringify({ name: user.name, email: user.email })), function(xhr) {
+          var res = JSON.parse(xhr.response)
+          localStorage.setItem('user', JSON.stringify(new UserDataModel(user.name, user.email, res._id, JSON.stringify(res))))
+          unload()
+          logIt()
+        })
+      }
+    } else {
+      document.getElementById('submit').onclick = function() {
+        setText('Singing...')
+        loadit()
+        db.get('account?q=' + decodeURI(JSON.stringify({ email: document.getElementById('email').value, password: document.getElementById('password').value })), function(xhr) {
+          console.log(res);
+          //localStorage.setItem('user', JSON.stringify(new UserDataModel(user.name, user.email, res._id, JSON.stringify(res))))
+          unload()
+        })
+      }
     }
-  } else {
-    
   }
 }
 
-function setText(txt){
+function setText(txt) {
   document.querySelector('.loadtitle').innerHTML = txt
 }
 
