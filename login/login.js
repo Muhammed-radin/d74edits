d7283fadb39504b2aac10a7e5727874dd2f1ab7c
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2023-07-21 23:41:58 +0530

    guests added

--- login/login.js
+++ login/login.js
@@ -1,7 +1,7 @@
-/*var user = JSON.parse(localStorage.getItem('user'))
+var user = JSON.parse(localStorage.getItem('user'))
 
 class UserDataModel {
-  constructor (name, email, id, data){
+  constructor(name, email, id, data) {
     this.name = name
     this.email = email
     this.id = id
@@ -12,30 +12,37 @@ class UserDataModel {
 if (localStorage.getItem('user')) {
   if (user.name.includes('guest') && user.email.includes('guest')) {
     if (user.id == undefined) {
+      setText('Updating...')
       loadit()
-      db.get('account?q=' + decodeURI(JSON.stringify({ name: user.name, email: user.email})), function(xhr){
+      db.get('account?q=' + decodeURI(JSON.stringify({ name: user.name, email: user.email })), function(xhr) {
         var res = JSON.parse(xhr.response)
         localStorage.setItem('user', JSON.stringify(new UserDataModel(user.name, user.email, res._id, JSON.stringify(res))))
         unload()
       })
     }
   } else {
-
+    
   }
-}*/
+}
 
-loadit()
+function setText(txt){
+  document.querySelector('.loadtitle').innerHTML = txt
+}
+
+document.querySelector('.block-div').style.zIndex = '9999';
 
 function loadit() {
   document.querySelector('.block-div').style.display = 'block';
-  document.querySelector('.center').style.display = 'block';
-  document.querySelector('.main-preload').style.display = 'block'; 
-  /*localStorage.setItem('account',{});*/
+
+  document.querySelectorAll('.center *').forEach(function(elem) {
+    elem.style.display = 'block'
+  })
 }
 
 function unload() {
   document.querySelector('.block-div').style.display = 'none';
-  document.querySelector('.center').style.display = 'none';
-  document.querySelector('.main-preload').style.display = 'none'; 
-  /*localStorage.setItem('account',{});*/
+
+  document.querySelectorAll('.center *').forEach(function(elem) {
+    elem.style.display = 'none'
+  })
 }
