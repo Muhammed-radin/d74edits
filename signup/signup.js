43b1f63bc63ce5276188e51946196e74d5881eb4
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2023-07-22 12:31:56 +0530

    restrictions added

--- signup/signup.js
+++ signup/signup.js
@@ -22,46 +22,45 @@ function logIt(lg = false) {
     }
   }
 
+  function $(id) {
+    return document.getElementById(id)
+  }
+
   document.getElementById('submit').addEventListener('click', function() {
-    setText('Singing...')
-    loadit()
-    db.get('account?q=' + decodeURI(JSON.stringify({ email: document.getElementById('email').value, password: document.getElementById('password').value })), function(xhr) {
-      var res = JSON.parse(xhr.response);
-      if (res.length == 0) {
-        alert('Signing Failed', 'Account not found, some reasons given below, type valid email and password <ul><li>Account Banned</li><li>or Account Deleted</li><li>or Invalid Email & Password</li><li>or Internet connection error</li></ul>')
-      } else {
-        localStorage.setItem('user', JSON.stringify(new UserDataModel(res[0].name, res[0].email, res[0]._id, JSON.stringify(res))))
-        redirctTo('../explore')
-      }
-
-      unload()
-    })
-  })
-
-  var params = location.search.replace('?', '');
-  var paramsString = '{"' + params.replaceAll("&", '","').replaceAll('=', '":"') + '"}';
-  if (params == '') {} else {
-    var objectParams = JSON.parse(paramsString);
-    objectParams = objectParams
-    if (objectParams.email == undefined || objectParams.password == undefined) {
-
+    if ($('username').value == false ||
+      $('password').value == false ||
+      $('confPass').value == false ||
+      $('email') == false) {
+      alert('please fill inputs')
+    } else if ($('email').value.includes('@') == false 
+    || $('email').value.includes('.') == false 
+    || $('email').value.slice($('email').value.lastIndexOf('.'), $('email').value.length).length >= 2){
+      alert('please type vail email')
+    } else if ($('password').value.length >= 8) {
+      alert('password required minimum 8 letters')
+    } else if ($('password').value == $('confPass').value){
+      alert('password and configure password is not equal')
     } else {
-      setText('Singing...')
-      loadit()
-      db.get('account?q=' + decodeURI(JSON.stringify({ email: document.getElementById('email').value, password: document.getElementById('password').value })), function(xhr) {
+    setText('Creating...')
+    loadit()
+    if (localStorage.getItem('user')) {
+      db.put('accounts/' + JSON.parse(localStorage.getItem('user')).id, JSON.stringify({
+        name: document.getElementByI('username').value,
+        password: document.getElementByI('password').value,
+        email: document.getElementByI('email').value,
+      }), function(xhr) {
         var res = JSON.parse(xhr.response);
         if (res.length == 0) {
           alert('Signing Failed', 'Account not found, some reasons given below, type valid email and password <ul><li>Account Banned</li><li>or Account Deleted</li><li>or Invalid Email & Password</li><li>or Internet connection error</li></ul>')
         } else {
           localStorage.setItem('user', JSON.stringify(new UserDataModel(res[0].name, res[0].email, res[0]._id, JSON.stringify(res))))
-          redirctTo(objectParams.url == undefined ? '../explore' : objectParams.url)
+          redirctTo('../explore')
         }
+
         unload()
       })
-
-    }
-  }
-
+    }}
+  })
 }
 
 function setText(txt) {
